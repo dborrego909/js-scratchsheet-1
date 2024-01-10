861e5d7e05c52ffc9e0952d9fc2311188eeb152e
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2024-01-09 18:52:14 -0800

    Scratch sheet for lessons.

---
+++ main.js
@@ -0,0 +1,30 @@
+const name = "John";
+let admin = name;
+// alert( admin );
+console.log("Hello World!");
+
+const homePlanet = "Earth";
+
+let currentVisitor = "John";
+
+console.log(23 + 97);
+
+console.log(4 + 9 + 7 + 3 + 4 + 9);
+
+let a = 10;
+
+console.log(a)
+
+console.log(9 * a);
+
+let b = 7 * a
+
+console.log(b);
+
+const MAX = 57;
+
+let actual = MAX - 13;
+
+let percentage = actual / MAX;
+
+console.log(percentage);
